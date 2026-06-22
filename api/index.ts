2315f1eabadcbaee9dd71f739456826c2b72a567
Abhiram Valmeekam <abhiram.valmeekam@example.com>
2026-06-22 05:47:11 +0530

    Add global polyfills for DOMMatrix, ImageData, and Path2D to fix pdf-parse crash in Vercel Serverless environment

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,15 @@
+// Polyfills for browser-focused dependencies on Vercel Serverless
+if (typeof (global as any).DOMMatrix === 'undefined') {
+  (global as any).DOMMatrix = class DOMMatrix {};
+}
+if (typeof (global as any).ImageData === 'undefined') {
+  (global as any).ImageData = class ImageData {};
+}
+if (typeof (global as any).Path2D === 'undefined') {
+  (global as any).Path2D = class Path2D {};
+}
+
 import app from '../server/src/server';
 
 export default app;
+
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,14 @@
+// Polyfills for browser-focused dependencies on Serverless environment
+if (typeof (global as any).DOMMatrix === 'undefined') {
+  (global as any).DOMMatrix = class DOMMatrix {};
+}
+if (typeof (global as any).ImageData === 'undefined') {
+  (global as any).ImageData = class ImageData {};
+}
+if (typeof (global as any).Path2D === 'undefined') {
+  (global as any).Path2D = class Path2D {};
+}
+
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
